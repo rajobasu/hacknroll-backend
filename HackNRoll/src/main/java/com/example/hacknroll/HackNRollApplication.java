ccb30daafecf4a9adba6f7f55b9ef602cba3332e
RajarshiBasu <rajobasu@localhost>
2021-01-08 16:11:18 +0800

    Basic Controller Added

--- HackNRoll/src/main/java/com/example/hacknroll/HackNRollApplication.java
+++ HackNRoll/src/main/java/com/example/hacknroll/HackNRollApplication.java
@@ -5,16 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication @RestController
+@SpringBootApplication
 public class HackNRollApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HackNRollApplication.class, args);
 	}
-	
-	@RequestMapping("/")
-	public String index() {
-		return "Greetings from Spring Boot!";
-	}
-	
 }
